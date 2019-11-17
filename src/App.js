814b309e86b571c2167cc2baf83594cf72e2448a
Roman <romangof@gmail.com>
2019-11-17 15:22:05 +0100

    remove reducer import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Grid, CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
